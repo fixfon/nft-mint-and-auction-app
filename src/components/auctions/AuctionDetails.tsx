dc0b154cb1ebec1116ed0b5f80d9e1e2648cd1e5
fixfon <mete.cts@gmail.com>
2022-11-10 01:17:07 +0300

    Added new button components to Auction page

--- src/components/auctions/AuctionDetails.tsx
+++ src/components/auctions/AuctionDetails.tsx
@@ -1,10 +1,16 @@
 import { ethers } from 'ethers';
 import moment from 'moment';
 import Image from 'next/image';
+import { useRouter } from 'next/router';
 import { useEffect, useState } from 'react';
-import { useContractRead } from 'wagmi';
+import { useAccount, useContractRead } from 'wagmi';
 import { AuctionContract } from '../../../contractAddress';
 import { Item } from '../../types/Item';
+import BidAuction from './BidAuction';
+import BuyNowAuction from './BuyNowAuction';
+import CancelAuction from './CancelAuction';
+import EndAuction from './EndAuction';
+import RefundBid from './RefundBid';
 
 interface AuctionDetailsProps {
   item: Item;
@@ -19,14 +25,15 @@ const AuctionDetails = ({ item, setItem, routeId }: AuctionDetailsProps) => {
     setCurrentTime(Date.now());
   }, []);
 
-  const [currentTime, setCurrentTime] = useState<number>();
+  const [currentTime, setCurrentTime] = useState<number>(0);
+  const { address } = useAccount();
+  const router = useRouter();
 
   const {
     data: auctionData,
     isError,
     isLoading,
     isSuccess,
-    error,
   } = useContractRead({
     address: AuctionContract,
     abi: [
@@ -114,15 +121,23 @@ const AuctionDetails = ({ item, setItem, routeId }: AuctionDetailsProps) => {
     ],
     args: [ethers.BigNumber.from(Number(routeId || -1))],
     functionName: 'getAuction',
-    watch: false,
+    watch: true,
     enabled: mounted && !!routeId,
     onSuccess: () => {
       if (auctionData) {
         const auctionItemData = {
           auctionId: auctionData.id.toString(),
           nftTokenId: Number(auctionData.nftTokenId.toString()),
-          highestBid: Number(auctionData.highestBid.toString()),
-          buyNowPrice: Number(auctionData.buyNowPrice.toString()),
+          seller: auctionData.seller,
+          startPrice: ethers.utils.formatEther(
+            auctionData.startPrice.toString()
+          ),
+          highestBid: ethers.utils.formatEther(
+            auctionData.highestBid.toString()
+          ),
+          buyNowPrice: ethers.utils.formatEther(
+            auctionData.buyNowPrice.toString()
+          ),
           endAt: Number(auctionData.endAt.toString()),
           isSold: auctionData.isSold,
           isCanceled: auctionData.isCanceled,
@@ -133,48 +148,102 @@ const AuctionDetails = ({ item, setItem, routeId }: AuctionDetailsProps) => {
     },
   });
 
+  useEffect(() => {
+    let tout: NodeJS.Timeout;
+    if (isError) {
+      tout = setTimeout(() => {
+        router.push('/auctions');
+      }, 3000);
+    }
+
+    return () => {
+      clearTimeout(tout);
+    };
+  }, [isError]);
+
   return (
     <div className="flex items-center justify-center">
       {mounted && (
         <>
-          {isError && <div>{error?.message}</div>}
-          {isLoading && <div>Loading...</div>}
+          {isError && (
+            <div>
+              <h2 className="text-2xl font-semibold text-neutral">
+                Auction does not exist. You are being redirected to auction list
+                in 3 seconds...
+              </h2>
+            </div>
+          )}
+          {isLoading && (
+            <div className="text-lg font-semibold text-neutral">Loading...</div>
+          )}
           {isSuccess && !!item.nftTokenId && (
-            <div
-              className="mt-4 flex min-w-fit max-w-2xl
-             flex-col items-center justify-center gap-8 rounded-lg border-4 border-highlight py-6 px-12 shadow-2xl md:flex-row md:px-20"
-            >
-              <Image
-                width={250}
-                height={250}
-                className="rounded-xl"
-                src={`/images/${item.nftTokenId}.png`}
-              />
-              <div className="flex h-[250px] flex-col items-center">
-                <div className="justify-self-start">
-                  <h2 className="text-2xl font-bold text-neutral">
-                    PatikaBears #{item.nftTokenId}
-                  </h2>
-                </div>
-                <div className="my-auto text-lg font-semibold text-neutral">
-                  <h3 className="py-2 text-2xl">
-                    Highest Bid:{' '}
-                    {ethers.utils.formatEther(item.highestBid.toString())} ETH
-                  </h3>
-                  <h3 className="pb-4">
-                    Buy Now:{' '}
-                    {ethers.utils.formatEther(item.buyNowPrice.toString())} ETH
-                  </h3>
-                  {!item.isEnded && (
-                    <h3>
-                      Ends:{' '}
-                      {moment
-                        .duration(moment(item.endAt, 'X').diff(currentTime))
-                        .humanize(true)}
+            <div className="mt-4 flex min-w-fit max-w-2xl flex-col items-center justify-center gap-8 rounded-lg border-4 border-highlight py-6 px-12 shadow-2xl md:px-20">
+              <div className="flex flex-col items-center justify-center gap-8 md:flex-row">
+                <Image
+                  width={250}
+                  height={250}
+                  className="rounded-xl"
+                  src={`/images/${item.nftTokenId}.png`}
+                />
+                <div className="flex h-[250px] flex-col items-center">
+                  <div className="justify-self-start">
+                    <h2 className="text-2xl font-bold text-neutral">
+                      PatikaBears #{item.nftTokenId}
+                    </h2>
+                  </div>
+                  <div className="my-auto text-lg font-semibold text-neutral">
+                    <h3 className="py-2 text-2xl">
+                      Highest Bid: {item.highestBid} ETH
                     </h3>
-                  )}
-                  <h3>Current Status: {item.isEnded ? 'Ended' : 'Active'}</h3>
+                    <h3>Buy Now: {item.buyNowPrice} ETH</h3>
+                    <h3 className="pb-4">Start Price: {item.startPrice} ETH</h3>
+                    {!item.isEnded && (
+                      <h3>
+                        Ends{' '}
+                        {moment
+                          .duration(moment(item.endAt, 'X').diff(currentTime))
+                          .humanize(true)}
+                      </h3>
+                    )}
+                    <h3>Current Status: {item.isEnded ? 'Ended' : 'Active'}</h3>
+                  </div>
                 </div>
+              </div>
+              <div className="">
+                {item.isEnded ? (
+                  <div>
+                    {item.isSold && (
+                      <h3 className="text-2xl font-bold text-neutral">
+                        This item is sold!
+                      </h3>
+                    )}
+                    {item.isCanceled && (
+                      <h3 className="text-2xl font-bold text-neutral">
+                        This item is canceled!
+                      </h3>
+                    )}
+                    {item.seller !== address && <RefundBid item={item} />}
+                  </div>
+                ) : (
+                  <div className="flex flex-col items-center justify-center gap-4">
+                    <>
+                      {item.seller === address ? (
+                        <>
+                          <EndAuction item={item} currentTime={currentTime} />
+                          <CancelAuction
+                            item={item}
+                            currentTime={currentTime}
+                          />
+                        </>
+                      ) : (
+                        <>
+                          <BidAuction item={item} />
+                          <BuyNowAuction item={item} />
+                        </>
+                      )}
+                    </>
+                  </div>
+                )}
               </div>
             </div>
           )}
